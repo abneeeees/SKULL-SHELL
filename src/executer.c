a8e59e5fcfa361cc82bd965727d98b7ced1516e2
abneeeees <avneeshsingh288@gmail.com>
2025-06-30 09:55:50 +0530

    contains the function that executes the shell commands and creates a parent/child process for external commands

---
+++ src/executer.c
@@ -0,0 +1,62 @@
+#include <executer.h>
+#include <commands.h>
+#include <parser.h>
+
+int checking_fork(char **args)
+{
+    pid_t pid = fork();
+    int status;
+
+    if (pid < 0 ){
+    printf("failed to fork");
+    exit (0) ;
+    } else if ( pid == 0){
+    //child process
+    if (execvp(args[0] , args) == -1){
+        // Command not found in built-ins or external
+        printf(RED "Error: wrong command\n" RESET);
+    } 
+    } else if (pid >0){
+        // Parent waits
+        waitpid(pid, &status, 0);
+
+        if (WIFEXITED(status) && WEXITSTATUS(status) == 0) {
+            return 0; 
+        } else {
+            return 1; 
+        }
+    }
+
+    return 1;
+
+}
+
+
+// Main function that executes commands
+int commd_execute(char **args) {
+    if (args[0] == NULL) {
+        // Empty command was passed
+        return 1;
+    }
+
+    // Loop through all built-in commands
+    for (int i = 0; i < builtin_size(); i++) {
+        // Compare first argument to built-in keywords
+        // strcmp(str1, str2) == 0 means strings are equal
+
+        if (strcmp(args[0], builtin[i]) == 0) {
+            // Call the corresponding built-in function
+            return (*builtin_func[i])(args);
+        }
+    }
+
+    // If we've reached the last builtin and no match, try external
+    if (checking_fork(args) == 0) {
+        return 1; // exits
+    }
+
+    return 1;
+}
+
+
+
