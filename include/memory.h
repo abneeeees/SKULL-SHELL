3232a19ece0a50578083701d762002c721576c8c
abneeeees <avneeshsingh288@gmail.com>
2025-07-04 23:44:07 +0530

    memory.h header file

---
+++ include/memory.h
@@ -0,0 +1,10 @@
+#ifndef MEMORY_H
+#define MEMORY_H
+
+#include <main.h>
+
+void store_history(char *line);
+//void up(FILE *ptr , char *line , int count);
+//void down(FILE *ptr , char *line , int count);
+
+#endif
