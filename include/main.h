ca5c037b715eb9324fe0d1b1767e31479d400a4d
abneeeees <avneeshsingh288@gmail.com>
2025-07-04 23:42:15 +0530

    added color function in main.h

--- include/main.h
+++ include/main.h
@@ -14,6 +14,17 @@
 #define BLUE    "\x1b[34m"
 #define MAGENTA "\x1b[35m"
 #define CYAN    "\x1b[36m"
+#define GRAY    "\x1b[30m"
 #define RESET   "\x1b[0m"
+
+#define red()     printf("\033[1;31m")
+#define green()   printf("\033[1;32m")
+#define yellow()  printf("\033[1;33m")
+#define blue()    printf("\033[1;34m")
+#define magenta() printf("\033[1;35m")
+#define cyan()    printf("\033[1;36m")
+#define gray()    printf("\033[1;30m")  // Bright black = gray
+#define reset()   printf("\033[0m")
+
  
 #endif
