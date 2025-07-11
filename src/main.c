5d2e7e36078ca4e5a4ff1476ed01b9c71b047dca
abneeeees <avneeshsingh288@gmail.com>
2025-07-11 22:13:25 +0530

    made some changes to interface

--- src/main.c
+++ src/main.c
@@ -9,46 +9,39 @@ void main_loop(void) {
     char *sentc;
     char **args;
     int status = 1;
-//
-//    FILE *ptr = fopen("commands.txt" , "r+");
-//    if (!ptr){perror("failed opening the file");}
-//    char *line = NULL;
-//    size_t len=0;
-//    int count=0;
-
 
     do {
+        time_t now = time(NULL);         // Get current time
+        struct tm *t = localtime(&now);  // Convert to local time structure
+
+        // printf("Year: %d\n", t->tm_year + 1900);  // Add 1900 to get the actual year
+        // printf("Month: %d\n", t->tm_mon + 1);     // Months are numbered from 0 to 11, so add 1 to match real month numbers (1-12)
+        // printf("Day: %d\n", t->tm_mday);
         char *username = getenv("USER");  //getting username of device
         char cd[1024];
         getcwd(cd, sizeof(cd));  // getting current directory of device
 
 
-        printf( YELLOW "%s " RESET , username  ); // printing username / current directory
-        printf(MAGENTA"[ %s ] " RESET , cd);
+        printf(MAGENTA" [ %s ] " RESET , cd);
+        printf(CYAN "[   %d : " , t->tm_hour);
+        printf(CYAN "%d | " , t->tm_min);
+        printf(CYAN "%d / ", t->tm_mon + 1);     // Months are numbered from 0 to 11, so add 1 to match real month numbers (1-12)
+        printf(CYAN "%d ]\n" RESET, t->tm_mday);
+        printf( YELLOW " %s " RESET , username  ); // printing username / current directory
         printf(GREEN">> "RESET);
 
         sentc = get_line();
         args = parser(sentc);
         status = commd_execute(args);
-//
-//	while(getline(&line , &len , ptr) != -1){
-//		if(strcmp(args[0] , "up") == 0){
-//			up(ptr , line , count);
-//		}
-//		if(strcmp(args[0] , "down") == 0){
-//			down(ptr , line , count);
-//		}
-
-//	}
         // freeing the dynamically allocated memory from get_line function and parser function
         free(sentc);
         free(args);
     } while (status);
 }
 
-
 int main() {
     // calling the main loop
+
     main_loop();
     return 0;
 }
