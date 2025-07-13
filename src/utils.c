92f5a539d295fac5bc789e2197d212c6db0fc0fb
abneeeees <avneeshsingh288@gmail.com>
2025-07-14 00:28:32 +0530

    added help function (working: h- [command])

--- src/utils.c
+++ src/utils.c
@@ -17,12 +17,8 @@ int cd(char **args) {
 int exit_shell(char **args) {
     if (args[0] == NULL) {
         perror("No command");
-    } else {
-        int i=1;
-        while (i<=4){
+    } else{
             exit(0) ; // exits the shell
-            i++ ;
-        } 
     }
 
     return 1;
@@ -68,5 +64,21 @@ int echo_commands(char **args) {
         }
         printf("\n") ;
     }
+
 return 1;
+}
+
+
+int help(char **args){
+    if (!args[0]){
+        perror("No command");
+    } else {
+        for (int i=0 ; i<builtin_size() ; i++){
+            if (strcmp(args[1] , builtin[i])==0){
+                printf("%s\n" , builtin_helps[i]) ;
+            }
+        }
+    }
+
+    return 1;
 }
