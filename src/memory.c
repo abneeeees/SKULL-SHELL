95bc64d2e4a3c8944cc71fc46ca1065c26cc699a
abneeeees <avneeshsingh288@gmail.com>
2025-07-14 00:42:26 +0530

    fixed bugs

--- src/memory.c
+++ src/memory.c
@@ -2,7 +2,7 @@
 
 void store_history(char* line){
 	FILE *ptr;
-	ptr = fopen("command.txt" , "a");
+	ptr = fopen("../history.txt" , "a");
 
 	if (ptr == NULL){
 		perror("failed to create file");
@@ -10,15 +10,7 @@ void store_history(char* line){
 
 	fprintf(ptr , "%s\n" , line);
 	fclose(ptr);
+
 }
-//
-//void up(FILE *ptr , char *line , int count){
-//	printf("%s" , line);
-//	line[count++];
-//}
-//
-//void down(FILE *ptr , char *line , int count){
-//	printf("%s" , line);
-//	line[count--];
-//}
-//
+
+
